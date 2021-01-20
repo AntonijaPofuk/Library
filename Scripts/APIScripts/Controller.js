57c57fe6622c66a84cdafe3600a20986bdaf96a0
AntonijaP <antonija.pofuk@gmail.com>
2021-01-20 21:48:27 +0100

    Created filter in AngularJS

--- Scripts/APIScripts/Controller.js
+++ Scripts/APIScripts/Controller.js
@@ -141,6 +141,23 @@
    
 })  
 
+app.filter('valueGreaterThan', function () {
+    return function (input, mark) {
+        var output = [];
+        if (isNaN(mark)) {
+            output = input;
+        }
+        else {
+            angular.forEach(input, function (item) {
+                if (item.Year > mark) {
+                    output.push(item)
+                }
+            });
+        }
+        return output;
+    }
+})
+
 app.factory('editDialog', ['$rootScope', '$compile', '$window', function ($rootScope, $compile, $window) {
     var html = '<edit-person sub="sub"></edit-person>';
     var link = $compile(html); //compilation happens only once
